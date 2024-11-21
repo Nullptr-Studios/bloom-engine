efff649cc5417792f907d55a9ba98e96e0fb45a9
Xein <xgonip@gmail.com>
2024-11-21 11:01:13 +0200

    Added modules

--- bloom/src/entrypoint.h
+++ bloom/src/entrypoint.h
@@ -8,11 +8,14 @@
 
 #pragma once
 #include "engine.h"
+#include <spdlog/spdlog.h>
 
 #ifdef BLOOM_PLATFORM_WINDOWS
 
 int main(int argc, char** argv) {
   auto _engine = bloom::CreateEngine();
+  spdlog::warn("Easy padding in numbers like {:08d}", 12);
+  spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
   _engine->Run();
   delete _engine;
 }
---
+++ bloom/src/logger.h
@@ -0,0 +1,19 @@
+/**
+ * \file logger.h
+ * \author Xein <xgonip@gmail.com>
+ * \date 11/21/2024
+ *
+ * \brief Wrapper for the log library
+ */
+
+#pragma once
+#include <spdlog/spdlog.h>
+
+namespace bloom {
+class Logger {
+
+  public:
+    Logger();
+  };
+
+} // namespace bloom
